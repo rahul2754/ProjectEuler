d445b5a669263c3cb6de5a601d30bd20dbdf0407
MrRahulRamkumar <rahulawesomeramkumar@gmail.com>
2017-09-05 21:54:33 +0530

    small edits

--- java/solutions/002.java
+++ java/solutions/002.java
@@ -2,19 +2,17 @@
 * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 * Find the sum of all the multiples of 3 or 5 below 1000.
 */
-<<<<<<< HEAD
+
 *
 *
 * @author MrRahulRamkumar
 */
-=======
 
 
 /**
  *
  * @author MrRahulRamkumar
  */
->>>>>>> 83b1f03296ac5003a77c5763f9de45890d22b983
 public class EvenFibonacci
 {
     public static void main (String [] args)
