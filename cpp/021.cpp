c345724454215f2cc87f04653776018976ccf9fe
Skrillex1169 <31664790+Skrillex1169@users.noreply.github.com>
2017-09-24 23:52:46 +0530

    Update 021.cpp
    Comments added to code.

--- cpp/021.cpp
+++ cpp/021.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 using namespace std;
 
-int div(int num)
+int div(int num)	//Function returns the value of the sums of perfect divisors of received parameter.
 {
 	int sum=0;
 	for(int i=1;i<num;i++)
 	{
-		if(num%i==0)
+		if(num%i==0)	//Checks perfect divisor.
 		{
-			sum=sum+i;
+			sum=sum+i;	//If found, increments the sum by the amount.
 		}
 	}
 	return sum;
@@ -18,16 +18,15 @@ int main()
 	int sum1=0,sum2=0,final_sum=0;
 	for(int i=1;i<10000;i++)
 	{
-		sum1=div(i);
-		sum2=div(sum1);
-		if(sum1==sum2)
+		sum1=div(i);	//The first number's sum of divisors.
+		sum2=div(sum1);	//The inital sum's own sum of divisors.
+		if(sum1==sum2) //By given condition, a!=b therefore is both sums are equal, move onto next part of the loop.
 		{
 			continue;
 		}
 		if(i==sum2)
 		{
-			final_sum= final_sum+sum2;
-		}
+			final_sum= final_sum+sum2;	//If the very number tested = it's divisor's sum's sum of divisors, then note d(b) = a when a!=b.
 	}
 	cout<<final_sum;
 }
